7b7ee98102054de584ca510fe8594d01e6be9fb3
Ekam-wadhwa1 <ekamwadhwa2004@gmail.com>
2026-01-01 21:23:59 +0530

    wrote down ideas and libraries

---
+++ code/esp32-code/esp32-code.ino
@@ -0,0 +1,16 @@
+//made by ekam wadhwa
+//leader of nexus group
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_MPU6050h>
+#include <Adafruit_Sensor.h>
+#include <MHZ19.h>
+#include <PMS.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <FastLED.h>
+#include <arduinoFFT.h>
+#include <Adafruit_CCS811.h>
